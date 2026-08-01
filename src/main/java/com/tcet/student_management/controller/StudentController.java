0096bae8a5e90762b4903ef4410cf2c256f68495
Ktiwari2310 <beingkt.23@gmail.com>
2026-08-01 14:40:25 +0530

    Patch

--- src/main/java/com/tcet/student_management/controller/StudentController.java
+++ src/main/java/com/tcet/student_management/controller/StudentController.java
@@ -3,12 +3,15 @@ package com.tcet.student_management.controller;
 import java.util.Optional;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import com.tcet.student_management.entity.Student;
 import com.tcet.student_management.repository.StudentRepository;
@@ -101,4 +104,26 @@ public StudentResponse deleteStudent(@PathVariable Long id) {
   }
   return new StudentResponse("Student not found", null);
 }
+
+@PatchMapping("/{id}")
+public ResponseEntity<StudentResponse>
+updateDepartment(
+    @PathVariable Long id,
+    @RequestBody Student studentDetails)
+{
+    Optional<Student> existingStudent =studentRepository.findById(id);
+        if(existingStudent.isPresent())
+                {
+                    Student student = existingStudent.get();student.setDepartment(studentDetails.getDepartment());  
+                    studentRepository.save(student);
+                    return ResponseEntity.ok(new StudentResponse(
+                        "Department Updated",
+                        student)
+                    );
+                } 
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new StudentResponse(
+                    "Student Not Found", 
+                    null));
 }
+}
+
